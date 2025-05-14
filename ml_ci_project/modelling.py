103874372cf60eba85b20f060201598aad5ba348
AdrianRamadhan <adrianramadhan881@gmail.com>
2025-05-14 21:41:00 +0700

    update ci

--- ml_ci_project/modelling.py
+++ ml_ci_project/modelling.py
@@ -13,19 +13,19 @@ if __name__ == "__main__":
 
     mlflow.set_experiment("MentalHealth_CI_Project")
 
-    with mlflow.start_run():
-        df_train = pd.read_csv(train_path)
-        df_test  = pd.read_csv(test_path)
-        X_train, y_train = df_train.drop("mental_health_risk", axis=1), df_train["mental_health_risk"]
-        X_test,  y_test  = df_test.drop("mental_health_risk", axis=1), df_test["mental_health_risk"]
+    # Tidak perlu mlflow.start_run() karena sudah dimulai otomatis oleh mlflow run
+    df_train = pd.read_csv(train_path)
+    df_test  = pd.read_csv(test_path)
+    X_train, y_train = df_train.drop("mental_health_risk", axis=1), df_train["mental_health_risk"]
+    X_test,  y_test  = df_test.drop("mental_health_risk", axis=1), df_test["mental_health_risk"]
 
-        mlflow.log_params({"n_estimators": n_estimators, "max_depth": max_depth})
-        model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, random_state=42)
-        model.fit(X_train, y_train)
+    mlflow.log_params({"n_estimators": n_estimators, "max_depth": max_depth})
+    model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, random_state=42)
+    model.fit(X_train, y_train)
 
-        preds = model.predict(X_test)
-        acc = accuracy_score(y_test, preds)
-        mlflow.log_metric("test_accuracy", acc)
-        mlflow.sklearn.log_model(model, "model")
+    preds = model.predict(X_test)
+    acc = accuracy_score(y_test, preds)
+    mlflow.log_metric("test_accuracy", acc)
+    mlflow.sklearn.log_model(model, "model")
 
-        print(f"Run finished. Test accuracy: {acc:.4f}")
+    print(f"Run finished. Test accuracy: {acc:.4f}")
